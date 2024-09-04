d7718937a6d889a991499690ca8a2b6dc6fd1e74
krivson <farelaranta.inbox@gmail.com>
2024-09-04 13:17:11 +0700

    Menambahkan beberapa konfigurasi cypress, dan memperbaiki beberapa script Create Bundling

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 8000,
+    pageLoadTimeout: 120000,
+    requestTimeout: 60000,
+    responseTimeout: 60000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/01-CreateBundling.cy.js
+++ cypress/e2e/01-CreateBundling.cy.js
@@ -1,74 +1,109 @@
 describe("Create Bundling", () => {
   const url = Cypress.env("url");
-  const users = Cypress.env("users");
-
+  const inputer = Cypress.env("inputer");
+  const approver = Cypress.env("approver");
   before(() => {
+    // Menghapus semua cookies
+    cy.clearCookies();
+
+    // Menghapus semua local storage
+    cy.clearLocalStorage();
+
+    // Menghapus semua session storage
+    cy.window().then((win) => {
+      win.sessionStorage.clear();
+    });
+  });
+
+  beforeEach(() => {
     cy.intercept(
       "POST",
-      "/feasibility/addon/composite/user/preauthenticate"
-    ).as("preAuthenticate");
-    cy.intercept("POST", "/feasibility/addon/composite/user/authenticate").as(
-      "authenticate"
+      "http://localhost:3000/feasibility/addon/composite/user/preauthenticate"
+    ).as("preauthenticate");
+
+    cy.intercept(
+      "POST",
+      "http://localhost:3000/feasibility/addon/composite/user/authenticate"
+    ).as("authenticate");
+    cy.intercept("POST", "http://localhost:3000/auth/enterprise-check").as(
+      "enterpriseCheck"
     );
-    cy.intercept("POST", "/auth/enterprise-check").as("enterpriseCheck");
-    cy.intercept("POST", "/feasibility/addon/composite/user/profile").as(
-      "getUserProfile"
+    cy.intercept(
+      "POST",
+      "http://localhost:3000/feasibility/addon/composite/user/profile"
+    ).as("userProfile");
+    cy.intercept("POST", "http://localhost:3000/auth/asbjorn-hromundr").as(
+      "asbjornHromundrAuth"
     );
-    cy.intercept("POST", "/auth/asbjorn-hromundr").as("asbjornHromundr");
     cy.intercept(
       "POST",
       "https://hasura-dev.apps.mypaas.telkom.co.id/v1/graphql"
-    ).as("graphqlQuery");
-    cy.intercept("GET", "/product/get-ebis-price-list").as("getEbisPriceList");
+    ).as("hasuraGraphql");
   });
 
   it("Create Hard Bundling", () => {
     cy.visit(url);
 
-    cy.get("#mui-1").type(users[0].username);
-    cy.get("#mui-2").type(users[0].password);
-    cy.get("#mui-3").click();
-    cy.wait("@preAuthenticate").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.get("#mui-4").type(users[0].otp);
-    cy.get("#mui-5").type(users[0].captcha);
-    cy.get("#mui-6").click();
-    cy.wait("@enterpriseCheck").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.wait("@getUserProfile").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.wait("@asbjornHromundr").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.wait("@getUserProfile").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.wait("@graphqlQuery").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(200);
+    cy.get("#mui-1").as("inputUsername");
+    cy.get("@inputUsername").type(inputer.username, { delay: 70 });
+
+    cy.get("#mui-2").as("inputPassword");
+    cy.get("@inputPassword").type(inputer.password, { delay: 70 });
+
+    cy.get("#mui-3").as("buttonOtp");
+    cy.get("@buttonOtp").click({ force: true });
+
+    cy.wait("@preauthenticate").its("response.statusCode").should("eq", 201);
+
+    cy.get("#mui-4").as("inputOtp");
+    cy.get("@inputOtp").type(inputer.otp, { delay: 70 });
+
+    cy.get("#mui-5").as("inputCaptcha");
+    cy.get("@inputCaptcha").type(inputer.captcha, { delay: 70 });
+
+    cy.get("#mui-6").as("buttonSignIn");
+    cy.get("@buttonSignIn").click();
+
+    cy.wait("@authenticate").its("response.statusCode").should("eq", 201);
+    cy.wait("@enterpriseCheck").its("response.statusCode").should("eq", 201);
+    cy.wait("@userProfile").its("response.statusCode").should("eq", 201);
+    cy.wait("@asbjornHromundrAuth")
+      .its("response.statusCode")
+      .should("eq", 201);
+    cy.wait("@hasuraGraphql").its("response.statusCode").should("eq", 200);
+
+    cy.get(".MuiPaper-elevation4 > .MuiToolbar-root").as("navigationBar");
+    cy.get("@navigationBar").contains("Manage Product").click();
+
+    cy.get(":nth-child(2) > .MuiMenuItem-root").as("listManageProduct");
+    cy.get("@listManageProduct").click();
+
+    cy.wait("@asbjornHromundrAuth")
+      .its("response.statusCode")
+      .should("eq", 201);
+    cy.wait("@hasuraGraphql").its("response.statusCode").should("eq", 200);
+
+    cy.get("#mui-28").as("inputPackageName");
+    cy.get("@inputPackageName").type("Bundling Pefita Dummy | 3555", {
+      delay: 70,
     });
 
-    // ---
+    cy.get("#mui-29").as("inputPackageType");
+    cy.get("@inputPackageType").type("Dummy", { delay: 70 });
 
-    cy.get(".MuiPaper-elevation4 > .MuiToolbar-root")
-      .contains("Manage Product")
-      .click();
-    cy.get(".MuiPaper-root > .MuiList-root").contains("Create Product").click();
-    cy.wait("@asbjornHromundr").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(201);
-    });
-    cy.wait("@graphqlQuery").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(200);
-    });
-    cy.get("#add-item-ebis").click();
-    cy.get(
-      ":nth-child(1) > .css-1jlvb0e-MuiStack-root > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > #asynchronous-demo"
-    ).type("Telkom Price List");
-    cy.wait("@getEbisPriceList").then((intercept) => {
-      expect(intercept.response.statusCode).to.eq(200);
-    });
-    cy.get("#asynchronous-demo-option-3").click();
+    cy.get("#mui-30").as("inputPackageGroup");
+    cy.get("@inputPackageGroup").type("Dummy", { delay: 70 });
+
+    cy.get("#mui-31").as("inputPackageDescription");
+    cy.get("@inputPackageDescription").type("Dummy", { delay: 70 });
+
+    cy.get("#mui-32").as("inputPackageSpeed");
+    cy.get("@inputPackageSpeed").type("100000", { delay: 70 });
+
+    cy.get("#add-item-ebis").as("addItemEbis");
+    cy.get("@addItemEbis").click();
+
+    // cy.get("#flag-bundle-select").as("selectFlagBundle");
+    // cy.get(".MuiList-root").as("listFlagBundle");
   });
 });
